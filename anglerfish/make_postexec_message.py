26a2666c6a95fd723c5b93aef53f67742dad989a
juancarlospaco <juancarlospaco@gmail.com>
2016-05-27 12:30:29 -0300

    fix post exec message

--- anglerfish/make_postexec_message.py
+++ anglerfish/make_postexec_message.py
@@ -20,15 +20,19 @@ except ImportError:
 def make_post_exec_msg(start_time=None, comment=None):
     """Simple Post-Execution Message with information about RAM and Time."""
     use, al, msg = 0, 0, ""
-    if sys.platform.startswith("linux"):
+    if sys.platform.startswith("windows"):
+        msg = "No information about RAM usage available on non-Linux-systems."
+    elif sys.platform.startswith("linux"):
         use = int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss *
                   resource.getpagesize() / 1024 / 1024 if resource else 0)
         al = int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') /
                  1024 / 1024 if hasattr(os, "sysconf") else 0)
-    msg += "Total Maximum RAM Memory use:{0} of {1}MegaBytes\n".format(use, al)
-    if start_time and datetime:
-        msg += "Total Working Time: {0}\n".format(datetime.now() - start_time)
+        msg += "Total Maximum RAM Memory used: {0} of {1}MegaBytes.\n".format(
+            use, al)
+        if start_time and datetime:
+            msg += "Total Working Time: ~ {0}.\n".format(
+                datetime.now() - start_time)
     if comment:
-        log.info(comment)
+        log.info(str(comment).strip())
     log.info(msg)
     return msg
