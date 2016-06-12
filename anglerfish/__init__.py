8db4e0042da77f66e1f1aaa2c0231abcf483d6b4
juancarlospaco <juancarlospaco@gmail.com>
2016-06-12 17:16:05 -0300

    Minor logger simplification

--- anglerfish/__init__.py
+++ anglerfish/__init__.py
@@ -134,7 +134,7 @@ def make_logger(name, when='midnight', single_zip=False):
         logging.StreamHandler.emit = add_color_emit_ansi(
             logging.StreamHandler.emit)
 
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
+    log.addHandler(logging.StreamHandler(sys.stderr))
     if os.path.exists("/dev/log") or os.path.exists("/var/run/syslog"):
         is_linux = sys.platform.startswith("linux")
         adrs = "/dev/log" if is_linux else "/var/run/syslog"
@@ -143,7 +143,7 @@ def make_logger(name, when='midnight', single_zip=False):
         except Exception:
             log.debug("Unix SysLog Server not found,ignore Logging to SysLog")
         else:
-            logging.getLogger().addHandler(handler)
+            log.addHandler(handler)
             log.debug("Unix SysLog Server trying to Log to SysLog: " + adrs)
     log.debug("Logger created with Log file at: {0}.".format(log_file))
     return log
