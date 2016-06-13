fe33349477e1e959b5076a4ee8d101c1532b98bc
juancarlospaco <juancarlospaco@gmail.com>
2016-06-13 11:38:18 -0300

    Add Notification support

---
+++ anglerfish/make_notification.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Notification message with information, based on D-Bus, with Fallbacks."""
+
+
+from shutil import which
+from subprocess import run
+
+try:
+    import dbus
+except ImportError:
+    dbus = None
+
+try:
+    import pynotify
+except ImportError:
+    pynotify = None
+
+
+def make_notification(title, message="", name="", icon="", timeout=3000):
+    """Notification message with information,based on D-Bus,with Fallbacks."""
+    if dbus:  # Theorically the standard universal way.
+        return dbus.Interface(dbus.SessionBus().get_object(
+            "org.freedesktop.Notifications", "/org/freedesktop/Notifications"),
+            "org.freedesktop.Notifications").Notify(
+                name, 0, icon, title, message, [], [], timeout)
+    elif pynotify:  # The non-standard non-universal way.
+        pynotify.init(name.lower() if name else title.lower())
+        return pynotify.Notification(title, message).show()
+    elif which("notify-send"):   # The non-standard non-universal sucky ways.
+        comand = (which("notify-send"), "--app-name=" + name,
+                  "--expire-time=" + str(timeout), title, message)
+        return run(comand, timeout=timeout // 1000 + 1)
+    elif which("kdialog"):
+        comand = (which("kdialog"), "--name=" + name, "--title=" + title,
+                  "--icon=" + icon, "--caption=" + name, "--passivepopup",
+                  title + message, str(timeout // 1000))
+        return run(comand, timeout=timeout // 1000 + 1)
+    elif which("zenity"):
+        comand = (which("zenity"), "--name=" + name, "--title=" + title,
+                  "--notification", "--timeout=" + str(timeout // 1000),
+                  "--text=" + title + message)
+        return run(comand, timeout=timeout // 1000 + 1)
+    elif which("xmessage"):  # This one is Ugly, but I hope you never get here.
+        comand = (which("xmessage"), "-center", title + message)
+        return run(comand, timeout=timeout // 1000 + 1)
