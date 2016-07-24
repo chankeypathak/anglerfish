9ba4e6aac37aa9af1bfeead7e3713598482dbe49
juancarlospaco <juancarlospaco@gmail.com>
2016-07-23 22:30:07 -0300

    Add Hardware Checks

---
+++ anglerfish/check_hardware.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Check if running on Battery, return Bool."""
+
+
+import os
+import dbus
+
+
+def _get_prop(obj, iface, prop):
+    """Get object interface properties."""
+    try:
+        return obj.Get(iface, prop, dbus_interface=dbus.PROPERTIES_IFACE)
+    except (dbus.DBusException, dbus.exceptions.DBusException) as err:
+        print(err)
+        return None
+
+
+def has_battery():
+    """Checks if we are connected to a AC power or Battery."""
+    battery_path = "/sys/class/power_supply"  # is it universal on Linux ?
+    if not os.path.exists(battery_path):
+        return False
+    for folder in os.listdir(battery_path):
+        type_path = os.path.join(battery_path, folder, 'type')
+        if os.path.exists(type_path):
+            with open(type_path) as power_file:
+                if power_file.read().lower().startswith('battery'):
+                    return True
+    return False
+
+
+def on_battery():
+    """Checks if we are running on Battery power."""
+    if has_battery():
+        bus, upower_path = dbus.SystemBus(), '/org/freedesktop/UPower'
+        upower = bus.get_object('org.freedesktop.UPower', upower_path)
+        result = _get_prop(upower, upower_path, 'OnBattery')
+        if result == None:  # Cannot read property, something is wrong.
+            print("Failed to read D-Bus property: {0}.".format(upower_path))
+            result = False  # Assume we are connected to a power supply.
+        return result
+    return False
+
+#
+# def on_wifi():
+#     """Checks if we are running on wifi."""
+#     try:
+#         bus = dbus.SystemBus()
+#         manager = bus.get_object('org.freedesktop.NetworkManager',
+#                                  '/org/freedesktop/NetworkManager')
+#          # FIXME this returns int, I dunno what they mean ?, investigate.
+#         return _get_prop(manager, 'org.freedesktop.NetworkManager',
+#                          'WirelessEnabled')
+#     except Exception:
+#         return False
