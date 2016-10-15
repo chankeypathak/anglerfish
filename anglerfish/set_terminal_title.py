3d0f01f6d6aa17ef91c7dc27247bc90ec456ed41
Anthony Fu <anthonyfu117@hotmail.com>
2016-10-16 03:35:20 +0800

    Make "set_terminal_title" works for Windows

--- anglerfish/set_terminal_title.py
+++ anglerfish/set_terminal_title.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 
 
-"""Set or Reset CLI Window Titlebar Title. Linux only?."""
+"""Set or Reset CLI Window Titlebar Title."""
 
+import os
+import sys
 
 def set_terminal_title(titlez=""):
-    """Set or Reset CLI Window Titlebar Title. Linux only?."""
-    if titlez and len(titlez.strip()) and isinstance(titlez, str):
-        print(r"\x1B]0; {0} \x07".format(titlez.strip()))
+    """Set or Reset CLI Window Titlebar Title."""
+    if titlez and isinstance(titlez, str) and len(titlez.strip()):
+        # Windows:
+        if sys.platform.startswith('win'):
+            os.system('title {0}'.format(titlez.strip()))
+        # Linux and otherwise
+        else:
+            print(r"\x1B]0; {0} \x07".format(titlez.strip()))
         return titlez
     else:
-        print(r"\x1B]0;\x07")
+        if sys.platform.startswith('win'):
+            os.system('title')
+        else:
+            print(r"\x1B]0;\x07")
