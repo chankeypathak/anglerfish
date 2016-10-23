8db4b9367874934a605d94c3310ecb0fd67f8864
juancarlospaco <juancarlospaco@gmail.com>
2016-10-22 23:44:11 -0300

    Use shutil.which() instead of Hardcoded path

--- anglerfish/set_terminal_title.py
+++ anglerfish/set_terminal_title.py
@@ -4,21 +4,23 @@
 
 """Set or Reset CLI Window Titlebar Title."""
 
+
 import os
 import sys
+from shutil import which
+
 
 def set_terminal_title(titlez=""):
     """Set or Reset CLI Window Titlebar Title."""
     if titlez and isinstance(titlez, str) and len(titlez.strip()):
-        # Windows:
-        if sys.platform.startswith('win'):
-            os.system('title {0}'.format(titlez.strip()))
-        # Linux and otherwise
-        else:
+        if sys.platform.startswith('win'):  # Windows:
+            os.system(which("title") + " {0}".format(titlez.strip()))
+        else:  # Linux, Os X and otherwise
             print(r"\x1B]0; {0} \x07".format(titlez.strip()))
         return titlez
     else:
         if sys.platform.startswith('win'):
-            os.system('title')
+            os.system(which("title"))
         else:
             print(r"\x1B]0;\x07")
+        return ""  # Title should be "" so we return ""
