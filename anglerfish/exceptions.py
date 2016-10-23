6bfd2eb7fbcc3438d034b9f3a19185703f768e9a
juancarlospaco <juancarlospaco@gmail.com>
2016-10-23 00:35:16 -0300

    PEP8, Minor

--- anglerfish/exceptions.py
+++ anglerfish/exceptions.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 """Common exceptions for anglerfish"""
+
 
 class AnglerfishException(Exception):
     pass
 
-class NamespaceConflictError(AnglerfishException):
+
+class NamespaceConflictError(ImportError, AnglerfishException):
     pass
--- anglerfish/path2import.py
+++ anglerfish/path2import.py
@@ -11,15 +11,17 @@ import os
 import errno
 from .exceptions import NamespaceConflictError
 
+
 def path2import(pat, name=None, ignore_exceptions=False, check_namespace=True):
     """Import a module from file path string.
 
     This is "as best as it can be" way to load a module from a file path string
     that I can find from the official Python Docs, for Python 3.5+."""
     module = None
-    if not os.path.exists(pat):
+    if not os.path.isfile(pat):
         if not ignore_exceptions:
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), pat)
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), pat)
     elif os.path.isdir(pat):
         if not ignore_exceptions:
             raise IsADirectoryError(pat)
@@ -29,27 +31,23 @@ def path2import(pat, name=None, ignore_exceptions=False, check_namespace=True):
     else:
         try:
             name = name or os.path.splitext(os.path.basename(pat))[0]
-
             if check_namespace and name in globals():
+                e = "Module {0} already exist on global namespace".format(name)
                 if not ignore_exceptions:
-                    raise NamespaceConflictError(
-                        'Module {name} already exists '
-                        'on global namespace.'.format(name=name))
+                    raise NamespaceConflictError(e)
             else:
                 spec = importlib.util.spec_from_file_location(name, pat)
                 if spec is None:
-                    # TODO: Change ImportError to a more proper Exception
+                    e = 'Failed to load module {0} from {1}.'.format(name, pat)
                     if not ignore_exceptions:
-                        raise ImportError('Failed to load module {0} from {1}'.format(name, pat))
+                        raise ImportError(e)
                 module = spec.loader.load_module()
-
         except Exception as error:
-            log.warning("Failed to Load Module {0} from {1}.".format(name, pat))
+            log.warning("Failed to Load Module {0} from {1}".format(name, pat))
             log.warning(error)
             if not ignore_exceptions:
                 raise
             module = None
         else:
-            log.debug("Loading Module {0} from file path {1}.".format(name, pat))
-
+            log.debug("Loading Module {0} from path {1}.".format(name, pat))
     return module
