eded1bcd8e36a76d496bb9ad2812af8d463a7b58
juancarlospaco <juancarlospaco@gmail.com>
2016-11-06 18:29:00 -0300

    Add NamedTuple to walk2list, optional.

--- anglerfish/walk2list.py
+++ anglerfish/walk2list.py
@@ -8,13 +8,24 @@
 import logging as log
 import os
 
+from typing import NamedTuple
 
-def walk2list(where, target, omit, links=False, tuply=True):
+
+def walk2list(where, target, omit, links=False, tuply=True, namedtuple=False):
     """Perform full walk of where, gather full path of all files."""
-    log.debug("Scan {},searching {},ignoring {}.".format(where, target, omit))
+    log.debug("Scan {},searching {},ignoring {},{}following SymLinks.".format(
+        where, target, omit, "" if links else "Not "))
     listy = [os.path.abspath(os.path.join(r, f))
              for r, d, fs in os.walk(where, followlinks=links)
              for f in fs if not f.startswith('.') and
              not f.endswith(omit) and
              f.endswith(target)]  # only target files,no hidden files
-    return tuple(listy) if tuply else listy
+    list_of_files = listy
+    if tuply:  # Return a Tuple.
+        list_of_files = tuple(listy)
+    if namedtuple:  # Return a NamedTuple with static typing.
+        namedtuple_of_files = NamedTuple(  # You can work comfortably using
+            "walk2list",  # walk2list.path_9 instead of walk2list[9]
+            fields=(("path_{0}".format(i), str) for i in range(len(listy))))
+        list_of_files = namedtuple_of_files(*listy)
+    return list_of_files
