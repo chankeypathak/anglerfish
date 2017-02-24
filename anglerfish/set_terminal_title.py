db625361946a29b9d354542c7e416e41264c370f
juancarlospaco <juancarlospaco@gmail.com>
2017-02-24 09:19:45 -0300

    Fix for MS Window

--- anglerfish/set_terminal_title.py
+++ anglerfish/set_terminal_title.py
@@ -13,13 +13,13 @@ from shutil import which
 def set_terminal_title(titlez=""):
     """Set or Reset CLI Window Titlebar Title."""
     if titlez and isinstance(titlez, str) and len(titlez.strip()):
-        if sys.platform.startswith('win'):  # Windows:
+        if sys.platform.startswith('win') and which("title"):  # Windows
             os.system(which("title") + " {0}".format(titlez.strip()))
         else:  # Linux, Os X and otherwise
             print(r"\x1B]0; {0} \x07".format(titlez.strip()))
         return titlez
     else:
-        if sys.platform.startswith('win'):
+        if sys.platform.startswith('win') and which("title"):
             os.system(which("title"))
         else:
             print(r"\x1B]0;\x07")
