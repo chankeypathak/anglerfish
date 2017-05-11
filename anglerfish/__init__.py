5a934393889c5e168724357c64d5a1093e4cf550
juancarlospaco <juancarlospaco@gmail.com>
2017-05-11 11:27:06 -0300

    Add AutoCheckSums.

--- anglerfish/__init__.py
+++ anglerfish/__init__.py
@@ -82,6 +82,7 @@ from anglerfish.get_random_font import (get_random_handwriting_font,
 from anglerfish.make_datauri import DataURI, img2webp  # noqa
 from anglerfish.get_human_datetime import get_human_datetime  # noqa
 from anglerfish.make_async import Sync2Async  # noqa
+from anglerfish.make_autochecksum import get_autochecksum, autochecksum  # noqa
 
 
 ##############################################################################
@@ -95,7 +96,7 @@ __url__ = 'https://github.com/juancarlospaco/anglerfish'
 __all__ = (
     'ChainableFuture', 'TemplatePython', 'Sync2Async',  # Those are Classes.
     'beep', 'bytes2human', 'check_encoding', 'check_folder', 'env2globals',
-    'get_clipboard', 'get_free_port',  # All functions.
+    'get_clipboard', 'get_free_port', 'get_autochecksum', 'autochecksum', # fun
     'get_temp_folder', 'get_zip_comment', 'has_battery', 'html2ebook',
     'ipdb_on_exception', 'json2xml', 'json_pretty', 'log_exception',
     'make_json_flat', 'make_logger', 'make_notification', 'make_post_exec_msg',
