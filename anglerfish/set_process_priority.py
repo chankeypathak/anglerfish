55e89ebb350114b2b7a39ff04a0810b4afe03139
juancarlospaco <juancarlospaco@gmail.com>
2017-05-11 16:38:44 -0300

    Add CPULimit. Fix #26

--- anglerfish/set_process_priority.py
+++ anglerfish/set_process_priority.py
@@ -5,27 +5,37 @@
 """Set process I/O and cpu priority."""
 
 
+import atexit
 import logging as log
 import os
 
 from shutil import which
-from subprocess import call
+from subprocess import call, Popen
 
 
-def set_process_priority(nice=True, ionice=False):
+def set_process_priority(nice=True, ionice=False, cpulimit=0):
     """Set process name and cpu priority."""
-    w = "IONice may delay I/O Operations, not recommended on user-facing GUI!."
+    w = " may delay I/O Operations, not recommended on user-facing GUI!."
     try:
         if nice:
             old = os.getpriority(os.PRIO_PROCESS, 0)
             os.nice(19)  # smooth cpu priority
             log.debug("Process CPUs Priority set: from {0} to 19.".format(old))
         elif ionice and which("ionice"):
-            log.warning(w)
+            log.warning("ionice" + w)
             command = "{0} --ignore --class 3 --pid {1}".format(
                 which("ionice"), os.getpid())
             call(command, shell=True)  # I/O nice,should work on Linux and Os X
             log.debug("Process I/O Priority set to: {0}.".format(command))
+        elif cpulimit and which("cpulimit"):
+            log.warning("cpulimit" + w)
+            log.debug("Launching 1 background 'cpulimit' child subprocess...")
+            cpulimit = int(cpulimit if cpulimit > 8 else 9)  # makes 9 the min.
+            command = "{0} --include-children --pid={1} --limit={2}".format(
+                which("cpulimit"), os.getpid(), cpulimit)
+            proces = Popen(command, shell=True)  # This launch a subprocess.
+            atexit.register(proces.kill)  # Force Kill subprocess at exit.
+            log.debug("Process CPUs Max Limits capped to: {0}".format(command))
     except Exception as error:
         log.warning(error)
         return False  # this may fail on windows and its normal, so be silent.
