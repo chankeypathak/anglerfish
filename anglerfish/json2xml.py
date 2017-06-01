d2df47020be40f540282ecbe1b1dac381f2a6628
juancarlospaco <juancarlospaco@gmail.com>
2017-06-01 16:43:11 -0300

    Micro-Optimization for Performance and Clean Ups.

--- anglerfish/json2xml.py
+++ anglerfish/json2xml.py
@@ -5,21 +5,21 @@
 """Convert JSON to XML."""
 
 
-def json2xml(json_obj, line_padding=""):
+def json2xml(json_obj, line_padding="", at_end=""):
     """Convert JSON to XML."""
     result_list, json_obj_type = [], type(json_obj)
-
+    _resultapend = result_list.append  # Optimization.
     if json_obj_type is list:
         for sub_elem in json_obj:
-            result_list.append(json2xml(sub_elem, line_padding))
+            _resultapend(json2xml(sub_elem, line_padding))
         return "\n".join(result_list)
 
     if json_obj_type is dict:
         for tag_name in json_obj:
             sub_obj = json_obj[tag_name]
-            result_list.append("{0}<{1}>".format(line_padding, tag_name))
-            result_list.append(json2xml(sub_obj, "    " + line_padding))
-            result_list.append("{0}</{1}>".format(line_padding, tag_name))
+            _resultapend("{0}<{1}>".format(line_padding, tag_name))
+            _resultapend(json2xml(sub_obj, "    " + line_padding))
+            _resultapend("{0}</{1}>{2}".format(line_padding, tag_name, at_end))
         return "\n".join(result_list)
 
     return "{0}{1}".format(line_padding, json_obj)
