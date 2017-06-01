6f0485c0611c73c7d3942c21408994051dd6bd14
juancarlospaco <juancarlospaco@gmail.com>
2017-06-01 16:42:54 -0300

    Micro-Optimization for Performance and Clean Ups.

--- anglerfish/bytes2human.py
+++ anglerfish/bytes2human.py
@@ -9,7 +9,7 @@ def bytes2human(bites, to, bsize=1024):
     """Convert bytes to kilobytes, megabytes, gigabytes, etc."""
     bitez, to = float(abs(bites)), str(to).strip().lower()
     for i in range({'k': 1, 'm': 2, 'g': 3, 't': 4, 'p': 5, 'e': 6}[to]):
-        bitez = bitez / int(abs(bsize))
-    return str(int(bitez)) + {
-        'k': " Kilo", 'm': " Mega", 'g': " Giga", 't': " Tera",
-        'p': " Peta", 'e': " Exa"}[to] + "bytes"
+        bitez = bitez / int(bsize)
+    return "{0} {1}bytes".format(int(bitez), {
+        'k': "Kilo", 'm': "Mega", 'g': "Giga",
+        't': "Tera", 'p': "Peta", 'e': "Exa"}[to])
