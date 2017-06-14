a2650bc01a06339b84fcaca3e99a71e36acb4214
juancarlospaco <juancarlospaco@gmail.com>
2017-06-14 12:38:42 -0300

    Use F-Strings,replace old str.format() calls.

--- anglerfish/make_json_flat.py
+++ anglerfish/make_json_flat.py
@@ -12,8 +12,7 @@ def make_json_flat(jsony, delimiter="__"):
         if isinstance(jsony[item], dict):
             get = make_json_flat(jsony[item], delimiter)
             for something in get.keys():
-                _key = "{0}{1}{2}".format(item, delimiter, something)
-                values[_key] = get[something]
+                values[f"{item}{delimiter}{something}"] = get[something]
         else:
             values[item] = jsony[item]
     return values
