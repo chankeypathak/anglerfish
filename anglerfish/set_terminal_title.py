9158d700603342e6ecc92209d19c2f30f2b43fdd
juancarlospaco <juancarlospaco@gmail.com>
2017-06-14 12:36:31 -0300

    Use F-Strings,replace old str.format() calls.

--- anglerfish/set_terminal_title.py
+++ anglerfish/set_terminal_title.py
@@ -15,13 +15,13 @@ def set_terminal_title(titlez=""):
     """Set or Reset CLI Window Titlebar Title."""
     if titlez and isinstance(titlez, str) and len(titlez.strip()):
         if sys.platform.startswith('win') and which("title"):  # Windows
-            run("{} {}".format(which("title"), titlez), shell=True, timeout=3)
+            run(f"{ which('title') } { titlez }", shell=True, timeout=9)
         else:  # Linux, Os X and otherwise
-            print(r"\x1B]0; {0} \x07".format(titlez.strip()))
+            print(f"\x1B]0; { titlez } \x07")
         return titlez
     else:
         if sys.platform.startswith('win') and which("title"):
-            run(str(which("title")), shell=True, timeout=3)
+            run(str(which("title")), shell=True, timeout=9)
         else:
             print(r"\x1B]0;\x07")
         return ""  # Title should be "" so we return ""
