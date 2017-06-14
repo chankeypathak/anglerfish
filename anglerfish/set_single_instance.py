5ebb093c6b5523e3832bfcbeb58e09169b569e97
juancarlospaco <juancarlospaco@gmail.com>
2017-06-14 12:36:38 -0300

    Use F-Strings,replace old str.format() calls.

--- anglerfish/set_single_instance.py
+++ anglerfish/set_single_instance.py
@@ -10,16 +10,15 @@ import socket
 import sys
 
 
-def set_single_instance(name, port=8888):
+def set_single_instance(name, port=8_888):
     """Set process name and cpu priority,return socket.socket or None."""
     try:  # Single instance app ~crossplatform, uses udp socket.
         log.info("Creating Abstract UDP Socket Lock for Single Instance.")
         __lock = socket.socket(
             socket.AF_UNIX if sys.platform.startswith("linux")
             else socket.AF_INET, socket.SOCK_STREAM)
-        __lock.bind(
-            "\0_{name}__lock".format(name=str(name).lower().strip())
-            if sys.platform.startswith("linux") else ("127.0.0.1", port))
+        __lock.bind(f"\0_{name}__lock" if sys.platform.startswith("linux")
+                    else ("127.0.0.1", port))
     except (socket.error, OSError, Exception) as e:
         __lock = None
         log.critical("Another instance of App is already running!, Exiting!.")
@@ -27,6 +26,6 @@ def set_single_instance(name, port=8888):
         sys.exit()
         exit()
     else:
-        log.info("Socket Lock for Single Instance: {}.".format(__lock))
+        log.info(f"Socket Lock for 1 Single Instance: {__lock!r}, {__lock}.")
     finally:
         return __lock
