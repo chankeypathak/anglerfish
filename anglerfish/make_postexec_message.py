aac4203a9dec7856c3f2ce84041843ce2664be76
juancarlospaco <juancarlospaco@gmail.com>
2017-06-14 12:38:15 -0300

    Use F-Strings,replace old str.format() calls.

--- anglerfish/make_postexec_message.py
+++ anglerfish/make_postexec_message.py
@@ -28,17 +28,17 @@ def make_post_exec_msg(start_time=None, comment=None):
     """Simple Post-Execution Message with information about RAM and Time."""
     use, al, msg = 0, 0, ""
     if sys.platform.startswith(("win", "darwin")):
-        msg = "No information about RAM usage available on non-Linux systems\n"
+        msg = "No information about RAM usage available on non-Linux systems."
     elif sys.platform.startswith("linux"):
         use = int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss *
                   resource.getpagesize() if resource else 0)
         al = int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
                  if hasattr(os, "sysconf") else 0)
-        msg += "Max Memory Used: {0} ({1} bytes) of {2} ({3} bytes).\n".format(
-            bytes2human(use, "m"), use, bytes2human(al, "m"), al)
+        msg += f"""Total Max Memory Used: {bytes2human(use, "m")} ({use} bytes)
+                of {bytes2human(al, "m")} ({al} bytes) of total RAM Memory\n"""
         if start_time and datetime:
             _t = datetime.now() - start_time
-            msg += "Working Time: {0} ({1}).\n".format(timedelta2human(_t), _t)
+            msg += f"Total Working Time: ~{ timedelta2human(_t) } ({ _t }).\n"
     if comment:
         msg += str(comment).strip()
     log.debug("Preparing Simple Post-Execution Messages.")
@@ -49,4 +49,4 @@ def make_post_exec_msg(start_time=None, comment=None):
 def app_is_ready(start_time):  # this goes at end of your apps __init__()
     """Simple Post-StartUp Message with Start-Up time."""
     _t = datetime.now() - start_time
-    log.debug("Total Start-Up Time: {0} ({1})".format(timedelta2human(_t), _t))
+    log.debug(f"Total app cold start-up time: ~{timedelta2human(_t)} ({_t}).")
