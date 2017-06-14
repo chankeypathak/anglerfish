5d5fc1b7bdf5eacc898f2e22601b07144a7c7a70
juancarlospaco <juancarlospaco@gmail.com>
2017-06-14 12:39:09 -0300

    Use F-Strings,replace old str.format() calls.

--- anglerfish/make_beep.py
+++ anglerfish/make_beep.py
@@ -22,11 +22,10 @@ def beep(waveform=(79, 45, 32, 50, 99, 113, 126, 127)):
         return False
     wavefile = gettempdir() / Path("beep.wav")
     repro = which("aplay" if sys.platform.startswith("linux") else "afplay")
-    log.debug("Playing Sound: {0} ({0!r}) using {1}.".format(wavefile, repro))
+    log.debug(f"Playing Sound Beep: {wavefile} ({wavefile!r}) using {repro}.")
     if not wavefile.is_file() or not os.access(wavefile.as_posix(), os.R_OK):
         with open(wavefile.as_posix(), "w+") as wave_file:
             for sample in range(0, 1000, 1):
                 for wav in range(0, 8, 1):
                     wave_file.write(chr(waveform[wav]))
-    return not bool(run("{repro} '{fyle}'".format(
-        fyle=wavefile.as_posix(), repro=repro), shell=True).returncode)
+    return not bool(run(f"{repro} '{wavefile}'", shell=True).returncode)
