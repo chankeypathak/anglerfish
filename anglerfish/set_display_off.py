d245b51d1c0db94280d28a94390317ac75286ebd
juancarlospaco <juancarlospaco@gmail.com>
2017-06-14 12:36:58 -0300

    Use F-Strings,replace old str.format() calls.

--- anglerfish/set_display_off.py
+++ anglerfish/set_display_off.py
@@ -15,9 +15,9 @@ from subprocess import run
 def set_display_off():
     """Set Monitor Display OFF, it should Auto-ON when needed, return Bool."""
     log.debug("Setting Monitor Display OFF.")
-    if sys.platform.startswith('linux') and which("xset"):
-        return not bool(
-            run(which("xset") + " dpms force off", shell=True).returncode)
+    _xset = which("xset")
+    if sys.platform.startswith('linux') and _xset:
+        return not bool(run(f"{_xset} dpms force off", shell=True).returncode)
     elif sys.platform.startswith('darwin'):
         return not bool(run(
             """echo 'tell application "Finder" to sleep' | osascript""",
