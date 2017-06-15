693d457a89553820e425430f681e6035498811dc
juancarlospaco <juancarlospaco@gmail.com>
2017-06-15 07:13:47 -0300

    Make Beep on MS Windows.

--- anglerfish/make_beep.py
+++ anglerfish/make_beep.py
@@ -14,12 +14,18 @@ from shutil import which
 from subprocess import run
 from tempfile import gettempdir
 
+try:
+    from winsound import Beep
+    from random import randint
+except ImportError:
+    Beep = None
+
 
 def beep(waveform=(79, 45, 32, 50, 99, 113, 126, 127)):
     """Cross-platform Sound Playing with StdLib only,No Sound file required."""
-    if sys.platform.startswith("win"):
-        log.warning("Playing Sound Natively is not supported by this OS.")
-        return False
+    if sys.platform.startswith("win") and Beep:
+        Beep(randint(37, 32_767), 3)
+        return True  # https://docs.python.org/3.6/library/winsound.html
     wavefile = gettempdir() / Path("beep.wav")
     repro = which("aplay" if sys.platform.startswith("linux") else "afplay")
     log.debug(f"Playing Sound Beep: {wavefile} ({wavefile!r}) using {repro}.")
