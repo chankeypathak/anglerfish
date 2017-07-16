cf376f0f2a26f10cb3eb31a531fcbabd3a338a90
juancarlospaco <juancarlospaco@gmail.com>
2017-07-16 15:58:47 -0300

    Minor.

--- setup.py
+++ setup.py
@@ -103,6 +103,7 @@ setup(
     classifiers=[
 
         'Development Status :: 5 - Production/Stable',
+        'Development Status :: 6 - Mature',
 
         'Environment :: Console',
         'Environment :: X11 Applications',
