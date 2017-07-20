8b8c5d385544b9ddc23c862fe4a1fa0f4d3f4d3b
juancarlospaco <juancarlospaco@gmail.com>
2017-07-20 16:25:47 -0300

    Refactor get_free_port to use OS-Provided Random free port or range from 2 integers tuple from args.

--- anglerfish/get_free_port.py
+++ anglerfish/get_free_port.py
@@ -9,17 +9,25 @@ import logging as log
 import socket
 
 
-def get_free_port(port_range=(8_000, 9_000)):
+def get_free_port(port_range=None):
     """Get and return a free unused port."""
     sockety = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sockety.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    for port_number in range(int(port_range[0]), int(port_range[1])):
-        try:
-            sockety.bind(("127.0.0.1", port_number))
-        except (socket.error, Exception):
-            pass
-        else:
-            sockety.close()
-            del sockety
-            log.debug(f"Found 1 free unused port number: {port_number}.")
-            return port_number
+    if port_range and isinstance(port_range, tuple) and len(port_range) == 2:
+        for port_number in range(int(port_range[0]), int(port_range[1])):
+            try:
+                sockety.bind(("127.0.0.1", port_number))
+            except (socket.error, Exception):
+                pass
+            else:
+                sockety.close()
+                del sockety
+                log.debug(f"Found 1 free unused port number: { port_number }.")
+                return port_number
+    else:
+        sockety.bind(("", 0))
+        port_number = sockety.getsockname()[1]
+        sockety.close()
+        del sockety
+        log.debug(f"Found 1 free unused port number: { port_number }.")
+        return port_number
