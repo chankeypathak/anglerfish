9a74d5f58ccfc37acf91f089737751a5983a91b4
juancarlospaco <juancarlospaco@gmail.com>
2017-08-05 03:09:30 -0300

    Add AutoSlots to protect our pretty classes.

---
+++ examples/slots.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Basic simple miminum possible examples of Angler AutoSlots."""
+
+
+from anglerfish import Autoslots_meta
+
+
+class TestinClass(metaclass=Autoslots_meta):
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+
+testin = TestinClass()
+print(testin.a, testin.b)
+testin.a, testin.b = 666, 42
+print(testin.a, testin.b)
+print(testin.__doc__)
+print(testin.__slots__)
+# This must raise:  AttributeError: 'TestinClass' object has no attribute 'c'.
+testin.c = "This should not be possible, because its protected by Angler!.  ;P"
