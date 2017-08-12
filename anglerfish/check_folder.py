34a3591da76a1dbbd0cc9b0f8fa5a0ed7e9003d4
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 05:02:25 -0300

    Add some Annotations for better selfdocumented code.

--- anglerfish/check_folder.py
+++ anglerfish/check_folder.py
@@ -16,7 +16,8 @@ except ImportError:
     from anglerfish import bytes2human
 
 
-def check_folder(folder_to_check=Path.home().as_posix(), check_space=1):
+def check_folder(folder_to_check: str=Path.home().as_posix(),
+                 check_space: int=1) -> bool:
     """Check working folder,from argument,for everything that can go wrong."""
     folder = Path(str(folder_to_check))
     m = "Folder {0} ({0!r}) free space {1} ({2} Bytes) of Minimum {3} GigaByte"
