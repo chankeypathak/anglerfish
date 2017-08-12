33342a903edfa8e9a4a331e488343d3502def010
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 11:14:05 -0300

    Fix that from and to argument can not be the same.

--- anglerfish/make_tinyslation.py
+++ anglerfish/make_tinyslation.py
@@ -18,6 +18,8 @@ def tinyslation(strin: str, to: str=getdefaultlocale()[0][:2],
                 frm: str="en", fallback_dict: dict={},
                 fallback_value: str=None, timeout: int=5) -> str:
     """Translate from internet via API from mymemory.translated.net,legally."""
+    if frm == to:  # if frm==to response 'PLEASE SELECT TWO DISTINCT LANGUAGES'
+        raise AttributeError(f"2 different languages required: {frm} == {to}.")
     st = parse.quote(strin)
     api = f"https://mymemory.translated.net/api/get?q={st}&langpair={frm}|{to}"
     req = request.Request(url=api, headers={'User-Agent': '', 'DNT': 1})
