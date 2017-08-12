113cf0ab1826da169ac9c12b8ff439e103874b90
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 04:33:29 -0300

    Simple comment, minor.

--- anglerfish/exceptions.py
+++ anglerfish/exceptions.py
@@ -7,14 +7,15 @@
 
 class AnglerfishException(Exception):
     """Common Exceptions for Anglerfish.
+
     Angler is a Simple Multipurpose Helper Utility Library for Python 3.6+ Apps
 
     For more info, docs, bugs, ideas, feedback and source code of Angler:
-    https://github.com/juancarlospaco/anglerfish#angler
+        https://github.com/juancarlospaco/anglerfish#angler
     Angler on PyPI:
-    https://pypi.python.org/pypi/anglerfish
+        https://pypi.python.org/pypi/anglerfish
     Angler on Arch:
-    https://aur.archlinux.org/packages/python-anglerfish
+        https://aur.archlinux.org/packages/python-anglerfish
 
     This Exception is meant to be used when Apps that use Angler fail.
     """
