c6c312397e52604139a6eca351db5361af83a336
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 07:52:42 -0300

    Use better argument names.

--- anglerfish/bytes2human.py
+++ anglerfish/bytes2human.py
@@ -8,17 +8,15 @@
 from math import log2
 
 
-def bytes2human(bites: int, to: str=None) -> str:
+def bytes2human(integer_bytes: int, to_unit: str=None) -> str:
     """Convert bytes to kilobytes, megabytes, gigabytes, etc."""
-    size = int(abs(bites))
+    size, to = int(abs(integer_bytes)), to_unit.lower() if to_unit else to_unit
+    sfx = ('', 'Kilo', 'Mega', 'Giga', 'Tera', 'Peta', 'Exa', 'Zetta', 'Yotta')
     if to:  # Force a specific data unit from args.
-        for i in range({'k': 1, 'm': 2, 'g': 3, 't': 4, 'p': 5, 'e': 6}[to]):
+        for i in range(sfx.index(to)):
             size = size / 1_024
-        unit, value = {'k': "Kilo", 'm': "Mega", 'g': "Giga",
-                       't': "Tera", 'p': "Peta", 'e': "Exa"}[to], int(size)
+        unit, value = to.title().strip(), int(size)
     else:  # Auto-scale data unit.
-        sfx = ('', 'Kilo', 'Mega', 'Giga', 'Tera',
-               'Peta', 'Exa', 'Zetta', 'Yotta')
         order = int(log2(size) / 10) if size else 0
         value, unit = int(size / (1 << (order * 10))), sfx[order]
     return f"{ value } { unit }bytes"
