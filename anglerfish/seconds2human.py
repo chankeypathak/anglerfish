89cee7e4d949d08d7fbe99f991a0721849552b43
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 04:36:29 -0300

    Full refactor to make the functions more useful and add more human frequent time units that can be useful to people, like centuries and decades, it returns a namedtuple which is more kind of self documenting, some nice comments to understand the code are also added, overall better api ux.

--- anglerfish/seconds2human.py
+++ anglerfish/seconds2human.py
@@ -5,36 +5,60 @@
 """Calculate time, with precision from seconds to days."""
 
 
-def seconds2human(time_on_seconds=0, do_year=True,
-                  unit_words={"y": " Years ", "d": " Days ", "h": " Hours ",
-                              "m": " Minutes ", "s": " Seconds "}):
+import time
+
+from collections import namedtuple
+
+
+def seconds2human(time_on_seconds: int):
     """Calculate time, with precision from seconds to days."""
-    minutes, seconds = divmod(int(abs(time_on_seconds)), 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    human_time_string = ""
-    yea = "{0}" + str(unit_words.get("y", " Years "))
-    dayz = "%02d" + str(unit_words.get("d", " Days "))
-    hou = "%02d" + str(unit_words.get("h", " Hours "))
-    minu = "%02d" + str(unit_words.get("m", " Minutes "))
-    seco = "%02d" + str(unit_words.get("s", " Seconds "))
-    years = int(int(days) / 365)
-    if days and years and do_year and years > 1:
-        human_time_string += yea.format(years)
-        days = days - (365 * years)
+    # Calculate all time units from time_on_seconds positive integer.
+    seconds = int(abs(time_on_seconds))  # 1 Seconds    = 1 Seconds.
+    minutes = divmod(seconds, 60)[0]     # 1 Minutes    = 60 Seconds.
+    hours = divmod(minutes, 60)[0]       # 1 Hours      = 60 Minutes.
+    days = divmod(hours, 24)[0]          # 1 Days       = 24 Hours.
+    weeks = divmod(days, 7)[0]           # 1 Weeks      = 7 Days.
+    months = divmod(weeks, 4)[0]         # 1 Months     = 4 Weeks.
+    years = divmod(months, 12)[0]        # 1 Years      = 12 Months.
+    decades = divmod(years, 10)[0]       # 1 Decade     = 10 Years.
+    centuries = divmod(decades, 10)[0]   # 1 Century    = 10 decades.
+
+    # Build a namedtuple with all named time units and all its integer values.
+    time_units = namedtuple(
+        "AllHumanFriendlyFrequentTimeUnits",
+        "seconds minutes hours days weeks months years decades centuries")(
+        seconds, minutes, hours, days, weeks, months, years, decades, centuries
+    )
+
+    # Build a human friendly time string with frequent time units.
+    human_time_parts = []
+    if centuries:
+        human_time_parts += f"{ centuries } Centuries"
+    if decades:
+        human_time_parts += f"{ decades } Decades"
+    if years:
+        human_time_parts += f"{ years } Years"
+    if months:
+        human_time_parts += f"{ months } Months"
+    if weeks:
+        human_time_parts += f"{ weeks } Weeks"
     if days:
-        human_time_string += dayz % days
+        human_time_parts += f"{ days } Days"
     if hours:
-        human_time_string += hou % hours
+        human_time_parts += f"{ hours } Hours"
     if minutes:
-        human_time_string += minu % minutes
-    human_time_string += seco % seconds
-    return human_time_string.strip()
+        human_time_parts += f"{ minutes } minutes"
+    human_time_parts += f"{ seconds } Seconds"
+    human_time = " ".join(human_time_parts)
+
+    return namedtuple("Times", "human_time time_units")(human_time, time_units)
 
 
-def timedelta2human(time_delta, do_year=True,  # Just a shortcut :)
-                    unit_words={"y": " Years ", "d": " Days ", "h": " Hours ",
-                                "m": " Minutes ", "s": " Seconds "}):
-    """Convert a TimeDelta object to human string representation."""
-    return seconds2human(time_delta.total_seconds(),
-                         do_year=do_year, unit_words=unit_words)
+def timedelta2human(time_delta):  # Just a shortcut :)
+    """Convert a timedelta object to human string representation."""
+    return seconds2human(time_delta.total_seconds())
+
+
+def datetime2human(date_time):  # Just a shortcut :)
+    """Convert a datetime object to human string representation."""
+    return seconds2human(time.mktime(date_time.timetuple()))
