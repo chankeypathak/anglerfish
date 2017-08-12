42f55f9248d42fc0682dafbb0db8e18ceb60bf92
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 04:32:55 -0300

    Simple comment, minor.

--- anglerfish/bytes2human.py
+++ anglerfish/bytes2human.py
@@ -11,12 +11,12 @@ from math import log2
 def bytes2human(bites, to=None):
     """Convert bytes to kilobytes, megabytes, gigabytes, etc."""
     size = int(abs(bites))
-    if to:
+    if to:  # Force a specific data unit from args.
         for i in range({'k': 1, 'm': 2, 'g': 3, 't': 4, 'p': 5, 'e': 6}[to]):
             size = size / 1_024
         unit, value = {'k': "Kilo", 'm': "Mega", 'g': "Giga",
                        't': "Tera", 'p': "Peta", 'e': "Exa"}[to], int(size)
-    else:
+    else:  # Auto-scale data unit.
         sfx = ('', 'Kilo', 'Mega', 'Giga', 'Tera',
                'Peta', 'Exa', 'Zetta', 'Yotta')
         order = int(log2(size) / 10) if size else 0
