9afcbec645c8301a54d4fe14997bb3da8f930efe
juancarlospaco <juancarlospaco@gmail.com>
2017-08-12 06:11:32 -0300

    Add some Annotations for better selfdocumented code.

--- anglerfish/make_template_python.py
+++ anglerfish/make_template_python.py
@@ -16,16 +16,16 @@ class TemplatePython(str):
 
     """Tiny Template Engine that Render and Runs native Python."""
 
-    def __init__(self, template):
+    def __init__(self, template: str):
         """Init the Template class."""
         self.tokens = self.compile(template.strip())
 
     @classmethod
-    def from_file(cls, fl):
+    def from_file(cls, fl: str) -> str:
         """Load template from file.A str/file-like object supporting read()."""
         return cls(Path(fl).read_text() if isinstance(fl, str) else fl.read())
 
-    def compile(self, t):
+    def compile(self, t: str) -> list:
         """Parse and Compile all Tokens found on the template string t."""
         tokens = []
         for i, p in enumerate(re.compile("\{\%(.*?)\%\}", re.DOTALL).split(t)):
@@ -41,7 +41,7 @@ class TemplatePython(str):
                 tokens.append((True, compile(tmplt, f"<tpl {tmplt}>", "exec")))
         return tokens
 
-    def render(__self, __namespace={}, mini=False, **kw):
+    def render(__self, __namespace: dict={}, mini: bool=False, **kw) -> str:
         """Render template from __namespace dict + **kw added to namespace."""
         html = []
         html_append = html.append  # Optimization.
