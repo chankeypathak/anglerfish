e353dd0fd8d4025929607ed46bb556c5d6978bb6
juancarlospaco <juancarlospaco@gmail.com>
2017-08-14 03:45:33 -0300

    Update Simplify bytes usability.

--- anglerfish/bytes2human.py
+++ anglerfish/bytes2human.py
@@ -5,18 +5,75 @@
 """Convert bytes to kilobytes, megabytes, gigabytes, etc."""
 
 
-from math import log2
+from collections import namedtuple, deque
 
 
-def bytes2human(integer_bytes: int, to_unit: str=None) -> str:
+def bytes2human(integer_bytes: int) -> namedtuple:
     """Convert bytes to kilobytes, megabytes, gigabytes, etc."""
-    size, to = int(abs(integer_bytes)), to_unit.lower() if to_unit else to_unit
-    sfx = ('', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa', 'zetta', 'yotta')
-    if to:  # Force a specific data unit from args.
-        for i in range(sfx.index(to)):
-            size = size / 1_024
-        unit, value = to.title().strip(), int(size)
-    else:  # Auto-scale data unit. ~1000 Yottabytes max capacity (IndexError).
-        order = int(log2(size) / 10) if size else 0
-        value, unit = int(size / (1 << (order * 10))), sfx[order].title()
-    return f"{ value } { unit }bytes"
+    # Calculate all Byte units from integer_bytes positive integer.
+    kilo, bites = divmod(int(abs(integer_bytes)), 1_024)
+    mega, kilo = divmod(kilo, 1_024)
+    giga, mega = divmod(mega, 1_024)
+    tera, giga = divmod(giga, 1_024)
+    peta, tera = divmod(tera, 1_024)
+    exa, peta = divmod(peta, 1_024)
+    zetta, exa = divmod(exa, 1_024)
+    yotta, zetta = divmod(zetta, 1_024)
+
+    # Build a namedtuple with all named bytes units and all its integer values.
+    bytes_units = namedtuple(
+        "AllHumanFriendlyFrequentByteUnits",
+        "byte kilo mega giga tera peta exa zetta yotta"
+    )(
+        bites, kilo, mega, giga, tera, peta, exa, zetta, yotta
+    )
+
+    # Build a human friendly bytes string with frequent bytes units.
+    bytes_parts, human_bytes_auto = deque(), None  # []
+    if yotta:
+        this_byte_unit = f"{yotta} Yottabyte{'s' if yotta > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{yotta}Yb"
+        bytes_parts.append(this_byte_unit)
+    if zetta:
+        this_byte_unit = f"{zetta} Zettabyte{'s' if zetta > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{zetta}Zb"
+        bytes_parts.append(this_byte_unit)
+    if exa:
+        this_byte_unit = f"{exa} Exabyte{'s' if exa > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{exa}Eb"
+        bytes_parts.append(this_byte_unit)
+    if peta:
+        this_byte_unit = f"{peta} Petabyte{'s' if peta > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{peta}Pb"
+        bytes_parts.append(this_byte_unit)
+    if tera:
+        this_byte_unit = f"{tera} Terabyte{'s' if tera > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{tera}Tb"
+        bytes_parts.append(this_byte_unit)
+    if giga:
+        this_byte_unit = f"{giga} Gigabyte{'s' if giga > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{giga}Gb"
+        bytes_parts.append(this_byte_unit)
+    if mega:
+        this_byte_unit = f"{mega} Megabyte{'s' if mega > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{mega}Mb"
+        bytes_parts.append(this_byte_unit)
+    if kilo:
+        this_byte_unit = f"{kilo} Kilobyte{'s' if kilo > 1 else ''}"
+        if not human_bytes_auto:
+            human_bytes_auto, bytes_short = this_byte_unit, f"{kilo}Kb"
+        bytes_parts.append(this_byte_unit)
+    if not human_bytes_auto:
+        human_bytes_auto = f"{bites} Byte{'s' if bites > 1 else ''}"
+        bytes_short = f"{bites}b"
+    bytes_parts.append(f"{bites} Byte{'s' if bites > 1 else ''}")
+
+    return namedtuple("HumanBytes", "human_bytes bytes_units auto short")(
+        " ".join(bytes_parts), bytes_units, human_bytes_auto, bytes_short)
