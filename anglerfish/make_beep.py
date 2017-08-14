892fa3c2771693f4c220a17bea17a5b954aaa7f2
juancarlospaco <juancarlospaco@gmail.com>
2017-08-13 23:30:30 -0300

    High performance speed optimization using C Deque.

--- anglerfish/make_beep.py
+++ anglerfish/make_beep.py
@@ -8,6 +8,8 @@
 import logging as log
 import os
 import sys
+
+from collections import deque
 from pathlib import Path
 from shutil import which
 from subprocess import run
@@ -25,12 +27,13 @@ def beep(waveform: tuple=(79, 45, 32, 50, 99, 113, 126, 127)) -> bool:
     if sys.platform.startswith("win") and Beep:
         Beep(randint(37, 32_767), 3)
         return True  # https://docs.python.org/3.6/library/winsound.html
-    wavefile = gettempdir() / Path("beep.wav")
+    wavefile, wavedeque = gettempdir() / Path("beep.wav"), deque()
     repro = which("aplay" if sys.platform.startswith("linux") else "afplay")
     log.debug(f"Playing Sound Beep: {wavefile} ({wavefile!r}) using {repro}.")
     if not wavefile.is_file() or not os.access(wavefile.as_posix(), os.R_OK):
-        with open(wavefile.as_posix(), "w+") as wave_file:
-            for sample in range(0, 1000, 1):
-                for wav in range(0, 8, 1):
-                    wave_file.write(chr(waveform[wav]))
+        wavedeque_append = wavedeque.append  # Optimization
+        for sample in range(0, 1000, 1):
+            for wav in range(0, 8, 1):
+                wavedeque_append(chr(waveform[wav]))
+        wavefile.write_bytes(*wavedeque)
     return not bool(run(f"{repro} '{wavefile}'", shell=True).returncode)
